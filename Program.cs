fa583a15aa77cb994caf51189934ebe0f746cd5a
jamalguliev <jamalvg@code.edu.az>
2021-12-05 23:25:07 +0400

    csharp

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "HelloWorld";
+            var arr = str.ToCharArray();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+    }
+    }
+
---
+++ Program2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            
+            
+
+            Student Student1 = new Student();
+            Student1.SetAge(21);
+            
+            Student1.ShowAge();
+            Student1.Study();
+
+            Teacher Teacher1 = new Teacher();
+            Teacher1.SetAge(30);
+            
+            Teacher1.Explain();
+        }
+    }
+
+    public class Person
+    {
+        protected int age;
+
+       
+
+        public void SetAge(int n)
+        {
+            age = n;
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining");
+        }
+    }
+    public class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm studying");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is {0} years old", age);
+        }
+    }
+
+    
+}
+
